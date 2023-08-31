2d488d93aa417dd7a154fb447e1c23be083eb639
Batyr <batyrlan2015@gmail.com>
2023-08-31 13:59:02 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,6 +62,7 @@ resource "aws_instance" "maintask" {
   instance_type          = var.instance_type
   subnet_id              = "subnet-0661942722a54dcf8"
   vpc_security_group_ids = [aws_security_group.security_terraform_batyrlan.id]
+  key_name               = var.keyname
   tags = var.common_tags
 }
 
